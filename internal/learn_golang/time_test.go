d58c94b6b9bedc7a4b2ad81c42c60d7938f553ce
Yifei <workeryifei@outlook.com>
2022-04-10 21:28:20 +0800

    测试time包，格式化以及时区比较

--- internal/learn_golang/time_test.go
+++ internal/learn_golang/time_test.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestTime(t *testing.T) {
@@ -21,5 +23,26 @@ func TestTime(t *testing.T) {
 
 	l2Time := time.Now().In(l2)
 	fmt.Println(l2Time.String())
+
+}
+
+func TestTimeLayout(t *testing.T) {
+	// 1、测试parse
+	// layout中缺失timezone时，默认使用UTC时间
+	parsedTime, err := time.Parse("2006-01-02 15:04:05", "2022-04-10 22:11:01")
+	if nil != err {
+		t.Error(err)
+	}
+	fmt.Println(parsedTime) // 输出的时间为UTC时间
+
+	// 2、测试parseInLocation
+	parsedTime2, err := time.ParseInLocation("2006-01-02 15:04:05", "2022-04-10 22:11:01", time.Local)
+	if nil != err {
+		t.Error(err)
+	}
+	fmt.Println(parsedTime2) // 输出的时间为Local时间,which is CST
+
+	eightHours, _ := time.ParseDuration("8h")
+	assert.Equal(t, eightHours, parsedTime.Sub(parsedTime2))
 
 }
