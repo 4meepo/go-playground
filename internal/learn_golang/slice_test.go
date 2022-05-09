29fd13058cab200b7a1b9cb509661d5e97debcc9
Yifei <workeryifei@outlook.com>
2022-05-09 15:53:30 +0800

    测试数组select

--- internal/learn_golang/slice_test.go
+++ internal/learn_golang/slice_test.go
@@ -24,10 +24,38 @@ func TestSlice(t *testing.T) {
 	intSlice2 := append(intSlice, 4)
 	assert.Len(t, intSlice, 3)
 	assert.Len(t, intSlice2, 4)
-
 }
 
 type Data struct {
 	a int
 	b string
+}
+
+func TestSelectSlice(t *testing.T) {
+	// 测试选择部分切片
+	sl := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	assert.Equal(t, 10, len(sl))
+
+	// 选择前两个
+	f2 := sl[:2]
+	assert.Equal(t, []int{0, 1}, f2)
+
+	// 选择最终两个
+	l2 := sl[len(sl)-2:]
+	assert.Equal(t, []int{8, 9}, l2)
+
+	// 未发生任何数组复制, 切片底层是同一个数组, 所以会影响原数组
+	l2[0] = 100
+	assert.Equal(t, 100, sl[8])
+
+	m2 := sl[5:7:10]
+	assert.Equal(t, 2, len(m2))
+	assert.Equal(t, 5, cap(m2))
+	assert.Equal(t, 5, m2[0])
+	assert.Equal(t, 6, m2[1])
+
+	// 如果访问数组下标超过len(), 会越界报错
+	m2[4] = 100
+	assert.Equal(t, 100, sl[9])
+
 }
