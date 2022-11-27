48d0234e26d5c21e97c41edd19f4695500aa40aa
Yifei <workeryifei@outlook.com>
2022-11-27 23:49:52 +0800

    test defer

--- internal/learn_golang/defer_test.go
+++ internal/learn_golang/defer_test.go
@@ -2,6 +2,7 @@ package learn_golang
 
 import (
 	"fmt"
+	"runtime"
 	"testing"
 )
 
@@ -12,4 +13,39 @@ func TestDefer(t *testing.T) {
 		}(i)
 	}
 
+}
+
+func TestDeferPointerRec(t *testing.T) {
+	a := &p2{"foo"}
+	defer a.NameP() // bar
+	defer a.Name()  // foo
+	a.name = "bar"
+
+	a = nil
+}
+
+type p2 struct {
+	name string
+}
+
+func (p *p2) NameP() {
+	fmt.Println(p.name)
+}
+func (p p2) Name() {
+	fmt.Println(p.name)
+}
+
+func TestDeferClosure(t *testing.T) {
+	var status string
+	var code int
+	defer func(code int) {
+		fmt.Println(status) // 引用的外部变量是在运行时才确定
+		fmt.Println(code)
+	}(code) // args 在闭包中是立即计算的
+	status = "foo"
+	code = 500
+	// output:
+	// foo
+	// 0
+	runtime.KeepAlive(code)
 }
--- internal/learn_golang/nil_test.go
+++ internal/learn_golang/nil_test.go
@@ -2,8 +2,9 @@ package learn_golang
 
 import (
 	"fmt"
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 // 参考 https://go.dev/doc/faq#nil_error
@@ -15,28 +16,29 @@ import (
 func TestNil(t *testing.T) {
 	err1 := GiveMeAError1()
 	fmt.Printf("err1: %T\n", err1)
-	assert.Equal(t, nil, err1)
+	assert.True(t, nil == err1)
 
 	err2 := GiveMeAError2()
 	fmt.Printf("err2: %T\n", err2)
-	assert.Equal(t, (*MyError)(nil), err2)
+	assert.True(t, nil == err2)
+	assert.True(t, (*MyError)(nil) == err2)
 
 	err3 := GiveMeAError3()
 	fmt.Printf("err3: %T\n", err3)
-	assert.Equal(t, (*MyError)(nil), err3)
+	assert.False(t, nil == err3)
 
 	// interface的zero value == nil
 	var err4 error
 	fmt.Printf("err4: %T\n", err4)
 	// In particular, a nil interface will always hold a nil type
 	assert.IsType(t, nil, err4)
-	assert.Equal(t, nil, err4)
+	assert.True(t, nil == err4)
 
 	// 指针类型的zero value != nil
 	var p1 *MyError
 	fmt.Printf("p1: %T\n", p1)
 	assert.IsType(t, (*MyError)(nil), p1)
-	assert.NotEqual(t, nil, p1)
+	assert.True(t, nil == p1)
 
 }
 
@@ -54,7 +56,6 @@ func GiveMeAError1() error {
 }
 
 func GiveMeAError2() *MyError {
-	// 这里的nil的type会被隐式转换为 *MyError 所以会发生 nil != nil
 	return nil
 }
 
@@ -62,4 +63,21 @@ func GiveMeAError3() error {
 	// 直接声明e，会导致e的type为*MyError 所以会发生 nil != nil
 	var e *MyError
 	return e
+}
+
+func TestNilPointer(t *testing.T) {
+	p := giveMeAPerson()
+	fmt.Println(p == nil)
+
+	e := GiveMeAError2()
+	fmt.Println(e == nil)
+}
+
+type person struct {
+	name string
+}
+
+func giveMeAPerson() *person {
+	var p *person
+	return p
 }
